2c75a5c3995f47a010fe621ed8bf9ab3379741e4
Udycz Alexis <udycz.alexis@gmail.com>
2025-07-16 13:28:51 +0200

    feat: :sparkles: time captures, deep sleep and saved in nvs

--- main/main.c
+++ main/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "esp_log.h"
 #include "esp_wifi.h"
@@ -9,8 +10,7 @@
 #include "esp_timer.h"
 #include "esp_event.h"
 #include "esp_sleep.h"
-
-#include "sys/param.h"
+#include "nvs.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -20,7 +20,7 @@
 #include "wifi_credentials.h"
 #include "main_wifi.h"
 
-// support IDF 5.x
+// Support IDF 5.x
 #ifndef portTICK_RATE_MS
 #define portTICK_RATE_MS portTICK_PERIOD_MS
 #endif
@@ -32,10 +32,8 @@
 // WROVER-KIT PIN Map
 #ifdef BOARD_WROVER_KIT
 
-#define TAG "main"
-
-#define CAM_PIN_PWDN -1  //power down is not used
-#define CAM_PIN_RESET -1 //software reset will be performed
+#define CAM_PIN_PWDN -1  // power down is not used
+#define CAM_PIN_RESET -1 // software reset will be performed
 #define CAM_PIN_XCLK 10
 #define CAM_PIN_SIOD 40
 #define CAM_PIN_SIOC 39
@@ -54,121 +52,64 @@
 
 #endif
 
-// ESP32Cam (AiThinker) PIN Map
-#ifdef BOARD_ESP32CAM_AITHINKER
-
-#define CAM_PIN_PWDN 32
-#define CAM_PIN_RESET -1 //software reset will be performed
-#define CAM_PIN_XCLK 0
-#define CAM_PIN_SIOD 26
-#define CAM_PIN_SIOC 27
-
-#define CAM_PIN_D7 35
-#define CAM_PIN_D6 34
-#define CAM_PIN_D5 39
-#define CAM_PIN_D4 36
-#define CAM_PIN_D3 21
-#define CAM_PIN_D2 19
-#define CAM_PIN_D1 18
-#define CAM_PIN_D0 5
-#define CAM_PIN_VSYNC 25
-#define CAM_PIN_HREF 23
-#define CAM_PIN_PCLK 22
-
-#endif
-// ESP32S3 (WROOM) PIN Map
-#ifdef BOARD_ESP32S3_WROOM
-#define CAM_PIN_PWDN 38
-#define CAM_PIN_RESET -1   //software reset will be performed
-#define CAM_PIN_VSYNC 6
-#define CAM_PIN_HREF 7
-#define CAM_PIN_PCLK 13
-#define CAM_PIN_XCLK 15
-#define CAM_PIN_SIOD 4
-#define CAM_PIN_SIOC 5
-#define CAM_PIN_D0 11
-#define CAM_PIN_D1 9
-#define CAM_PIN_D2 8
-#define CAM_PIN_D3 10
-#define CAM_PIN_D4 12
-#define CAM_PIN_D5 18
-#define CAM_PIN_D6 17
-#define CAM_PIN_D7 16
-#endif
-// ESP32S3 (GOOUU TECH)
-#ifdef BOARD_ESP32S3_GOOUUU
-#define CAM_PIN_PWDN -1
-#define CAM_PIN_RESET -1   //software reset will be performed
-#define CAM_PIN_VSYNC 6
-#define CAM_PIN_HREF 7
-#define CAM_PIN_PCLK 13
-#define CAM_PIN_XCLK 15
-#define CAM_PIN_SIOD 4
-#define CAM_PIN_SIOC 5
-#define CAM_PIN_D0 11
-#define CAM_PIN_D1 9
-#define CAM_PIN_D2 8
-#define CAM_PIN_D3 10
-#define CAM_PIN_D4 12
-#define CAM_PIN_D5 18
-#define CAM_PIN_D6 17
-#define CAM_PIN_D7 16
-#endif
-
 #define MQTT_BROKER_URI "mqtt://broker.hivemq.com"
-#define MQTT_BROKER_TOPIC "IeziPVyPNYnaOW4U/"
+// #define MQTT_BROKER_TOPIC_TIME "IeziPVyPNYnaOW4U/time"
+#define MQTT_BROKER_TOPIC_IMAGE "IeziPVyPNYnaOW4U/image"
+#define MQTT_BROKER_TOPIC_AVERAGE "IeziPVyPNYnaOW4U/average"
 
 #define TAG "main"
-#define MAX_MEASUREMENTS 100
-#define NVS_NAMESPACE "measurements"
-#define NVS_KEY_COUNTER "counter"
+#define TIMING_SAMPLES 100
 
-int64_t wifi_connect_times[MAX_MEASUREMENTS];
-int64_t mqtt_connect_times[MAX_MEASUREMENTS];
-int64_t capture_send_times[MAX_MEASUREMENTS];
+typedef struct {
+    int64_t wifi_time;
+    int64_t mqtt_connect_time;
+    int64_t mqtt_publish_time;
+    int64_t capture_time;
+} timing_data_t;
 
-int measurement_index = 0;
+static int64_t mqtt_connect_start_time = 0;
+static int64_t mqtt_connect_end_time = 0;
+static int32_t sample_count = 0;
+
+// Function declarations
+void save_timing_to_nvs(const timing_data_t *data, int index);
+void load_timing_from_nvs(timing_data_t *data, int index);
+void reset_nvs_samples(void);
 
 #if ESP_CAMERA_SUPPORTED
-static camera_config_t camera_config = {
-    .pin_pwdn = CAM_PIN_PWDN,
-    .pin_reset = CAM_PIN_RESET,
-    .pin_xclk = CAM_PIN_XCLK,
-    .pin_sccb_sda = CAM_PIN_SIOD,
-    .pin_sccb_scl = CAM_PIN_SIOC,
-
-    .pin_d7 = CAM_PIN_D7,
-    .pin_d6 = CAM_PIN_D6,
-    .pin_d5 = CAM_PIN_D5,
-    .pin_d4 = CAM_PIN_D4,
-    .pin_d3 = CAM_PIN_D3,
-    .pin_d2 = CAM_PIN_D2,
-    .pin_d1 = CAM_PIN_D1,
-    .pin_d0 = CAM_PIN_D0,
-    .pin_vsync = CAM_PIN_VSYNC,
-    .pin_href = CAM_PIN_HREF,
-    .pin_pclk = CAM_PIN_PCLK,
-
-    //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
-    .xclk_freq_hz = 20000000,
-    .ledc_timer = LEDC_TIMER_0,
-    .ledc_channel = LEDC_CHANNEL_0,
-
-    .pixel_format = PIXFORMAT_JPEG, //YUV422,GRAYSCALE,RGB565,JPEG
-    .frame_size = FRAMESIZE_QVGA,    //QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
-
-    .jpeg_quality = 12, //0-63, for OV series camera sensors, lower number means higher quality
-    .fb_count = 1,       //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
-    .fb_location = CAMERA_FB_IN_PSRAM,
-    .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
-};
-
 static esp_err_t init_camera(void)
 {
-    //initialize the camera
-    esp_err_t err = esp_camera_init(&camera_config);
-    if (err != ESP_OK)
-    {
+    camera_config_t config = {
+        .pin_pwdn = CAM_PIN_PWDN,
+        .pin_reset = CAM_PIN_RESET,
+        .pin_xclk = CAM_PIN_XCLK,
+        .pin_sccb_sda = CAM_PIN_SIOD,
+        .pin_sccb_scl = CAM_PIN_SIOC,
+        .pin_d7 = CAM_PIN_D7,
+        .pin_d6 = CAM_PIN_D6,
+        .pin_d5 = CAM_PIN_D5,
+        .pin_d4 = CAM_PIN_D4,
+        .pin_d3 = CAM_PIN_D3,
+        .pin_d2 = CAM_PIN_D2,
+        .pin_d1 = CAM_PIN_D1,
+        .pin_d0 = CAM_PIN_D0,
+        .pin_vsync = CAM_PIN_VSYNC,
+        .pin_href = CAM_PIN_HREF,
+        .pin_pclk = CAM_PIN_PCLK,
+
+        .xclk_freq_hz = 20000000,
+        .ledc_timer = LEDC_TIMER_0,
+        .ledc_channel = LEDC_CHANNEL_0,
+        .pixel_format = PIXFORMAT_JPEG,
+        .frame_size = FRAMESIZE_QVGA,
+        .jpeg_quality = 12,
+        .fb_count = 1,
+        .fb_location = CAMERA_FB_IN_PSRAM,
+        .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
+    };
+
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK) {
         ESP_LOGE(TAG, "Camera Init Failed");
         return err;
     }
@@ -182,12 +123,11 @@ static void mqtt_event_handler_cb(void *handler_args, esp_event_base_t base, int
     ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32 "", base, event_id);
     esp_mqtt_event_handle_t event = event_data;
     esp_mqtt_client_handle_t client = event->client;
-    int msg_id;
 
     switch ((esp_mqtt_event_id_t)event_id) {
         case MQTT_EVENT_CONNECTED:
+            mqtt_connect_end_time = esp_timer_get_time();
             ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-            msg_id = esp_mqtt_client_publish(client, MQTT_BROKER_TOPIC, "Hello from ESP32S3!", 0, 0, 0);
             break;
         case MQTT_EVENT_DISCONNECTED:
             ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
@@ -201,82 +141,237 @@ static void mqtt_event_handler_cb(void *handler_args, esp_event_base_t base, int
 static esp_mqtt_client_handle_t mqtt_app_start(void)
 {
     esp_mqtt_client_config_t mqtt_cfg = {
-        .broker.address.uri = MQTT_BROKER_URI, // Public broker for testing
+        .broker.address.uri = MQTT_BROKER_URI,
     };
     esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
-    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler_cb, client);
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler_cb, NULL);
     esp_mqtt_client_start(client);
-    
     return client;
-}
-
-static void camera_loop(esp_mqtt_client_handle_t client)
-{
-#if ESP_CAMERA_SUPPORTED
-    if(ESP_OK != init_camera()) {
-        return;
-    }
-
-    while (1)
-    {
-        ESP_LOGI(TAG, "Taking picture...");
-        camera_fb_t *pic = esp_camera_fb_get();
-
-        esp_mqtt_client_publish(client, MQTT_BROKER_TOPIC, (const char *)pic->buf, pic->len, 0, 0);
-
-        ESP_LOGI(TAG, "Picture taken! Its size was: %zu bytes", pic->len);
-        esp_camera_fb_return(pic);
-
-        vTaskDelay(5000 / portTICK_RATE_MS);
-    }
-#else
-    ESP_LOGE(TAG, "Camera support is not available for this chip");
-    return;
-#endif
 }
 
 void app_main(void)
 {
-	int64_t wifi_start_time, wifi_end_time;
+    int64_t wifi_start_time = 0, wifi_end_time = 0;
+    int64_t mqtt_start_time = 0, mqtt_end_time = 0;
+    int64_t capture_start_time = 0, capture_end_time = 0;
+    esp_mqtt_client_handle_t client = NULL;
+
+    bool sent_successfully = false;
+    timing_data_t current_timing = {0};
 
     ESP_LOGI(TAG, "Starting...");
+
+    // Initialize NVS
+    ESP_ERROR_CHECK(nvs_flash_init());
+
+    // Load current sample count from NVS
+    nvs_handle_t nvs;
+    ESP_ERROR_CHECK(nvs_open("timing", NVS_READWRITE, &nvs));
     
-	wifi_start_time = esp_timer_get_time();
+    esp_err_t err = nvs_open("timing", NVS_READWRITE, &nvs);
+    if (err == ESP_OK) {
+        err = nvs_get_i32(nvs, "sample_count", &sample_count);
+        if (err != ESP_OK) {
+            ESP_LOGW(TAG, "sample_count not found in NVS, initializing to 0");
+            sample_count = 0;
+        }
+        nvs_close(nvs);
+    } else {
+        ESP_LOGE(TAG, "Failed to open NVS handle, initializing sample_count to 0");
+        sample_count = 0;
+    }
 
-	ESP_LOGI(TAG, "WiFi connection established in : %lld milliseconds", wifi_start_time/1000);
+    nvs_close(nvs);
 
+    if (sample_count >= TIMING_SAMPLES) {
+        ESP_LOGI(TAG, "Already collected 100 samples. Resetting...");
+        reset_nvs_samples();
+        sample_count = 0;
+    }
+
+    // Initialize Wi-Fi
     ESP_ERROR_CHECK(app_wifi_init());
 
-    esp_err_t ret = app_wifi_connect(WIFI_SSID, WIFI_PASSWORD);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to connect to Wi-Fi network");
+    wifi_start_time = esp_timer_get_time();
+
+    // Try connecting to Wi-Fi
+    if (app_wifi_connect(WIFI_SSID, WIFI_PASSWORD) != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to connect to Wi-Fi.");
+        goto cleanup;
     }
 
-	wifi_end_time = esp_timer_get_time();
-    wifi_connect_times[measurement_index] = wifi_end_time - wifi_start_time;
+    wifi_end_time = esp_timer_get_time();
+    ESP_LOGI(TAG, "WiFi connected.");
 
-    ESP_LOGI(TAG, "WiFi connected in %.2f ms", (float) capture_send_times[measurement_index]/1000);
+    
+    // Start MQTT client
+    mqtt_connect_start_time = esp_timer_get_time();
+    client = mqtt_app_start();
 
-    wifi_ap_record_t ap_info;
-    ret = esp_wifi_sta_get_ap_info(&ap_info);
-    if (ret == ESP_ERR_WIFI_CONN) {
-        ESP_LOGE(TAG, "Wi-Fi station interface not initialized");
-    }
-    else if (ret == ESP_ERR_WIFI_NOT_CONNECT) {
-        ESP_LOGE(TAG, "Wi-Fi station is not connected");
-    } else {
-        ESP_LOGI(TAG, "--- Access Point Information ---");
-        ESP_LOG_BUFFER_HEX("MAC Address", ap_info.bssid, sizeof(ap_info.bssid));
-        ESP_LOG_BUFFER_CHAR("SSID", ap_info.ssid, sizeof(ap_info.ssid));
-        ESP_LOGI(TAG, "Primary Channel: %d", ap_info.primary);
-        ESP_LOGI(TAG, "RSSI: %d", ap_info.rssi);
-
-        camera_loop(mqtt_app_start());
+#if ESP_CAMERA_SUPPORTED
+    if (init_camera() != ESP_OK) {
+        ESP_LOGE(TAG, "Camera Init Failed");
+        goto cleanup;
     }
 
+    ESP_LOGI(TAG, "Taking picture...");
+    capture_start_time = esp_timer_get_time();
+    camera_fb_t *pic = esp_camera_fb_get();
+    capture_end_time = esp_timer_get_time();
+
+    if (!pic) {
+        ESP_LOGE(TAG, "Camera capture failed");
+        goto cleanup;
+    }
+
+    ESP_LOGI(TAG, "Picture taken in %.2f ms", (float)(capture_end_time - capture_start_time) / 1000);
+
+    mqtt_start_time = esp_timer_get_time();
+    int msg_id = esp_mqtt_client_publish(client, MQTT_BROKER_TOPIC_IMAGE, (const char *)pic->buf, pic->len, 0, 0);
+    mqtt_end_time = esp_timer_get_time();
+    ESP_LOGI(TAG, "Image sent with msg_id %d in %.2f ms", msg_id, (float)(mqtt_end_time - mqtt_start_time) / 1000);
+
+    esp_camera_fb_return(pic);
+
+    // Save timing data
+    current_timing.wifi_time = wifi_end_time - wifi_start_time;
+    current_timing.mqtt_publish_time = mqtt_end_time - mqtt_start_time;
+    current_timing.mqtt_connect_time = mqtt_connect_end_time - mqtt_connect_start_time;
+    current_timing.capture_time = capture_end_time - capture_start_time;
+
+    save_timing_to_nvs(&current_timing, sample_count);
+    sample_count++;
+
+    // Update sample count in NVS
+    ESP_ERROR_CHECK(nvs_open("timing", NVS_READWRITE, &nvs));
+    ESP_ERROR_CHECK(nvs_set_i32(nvs, "sample_count", sample_count));
+    ESP_ERROR_CHECK(nvs_commit(nvs));
+    nvs_close(nvs);
+
+    ESP_LOGI(TAG, "Sample #%" PRId32 " stored.", sample_count);
+
+    // If 100 samples collected, compute average and send
+    if (sample_count == TIMING_SAMPLES) {
+        int64_t total_wifi = 0, total_mqtt_publish = 0, total_mqtt_connect = 0, total_capture = 0;
+
+        timing_data_t temp;
+        for (int i = 0; i < TIMING_SAMPLES; i++) {
+            load_timing_from_nvs(&temp, i);
+            total_wifi += temp.wifi_time;
+            total_mqtt_connect += temp.mqtt_connect_time;
+            total_mqtt_publish += temp.mqtt_publish_time;
+            total_capture += temp.capture_time;
+        }
+
+        float avg_wifi = (float)total_wifi / TIMING_SAMPLES / 1000;
+        float avg_mqtt_connect = (float)total_mqtt_connect / TIMING_SAMPLES / 1000;
+        float avg_mqtt_publish = (float)total_mqtt_publish / TIMING_SAMPLES / 1000;
+        float avg_capture = (float)total_capture / TIMING_SAMPLES / 1000;
+
+        char avg_json[256];
+        snprintf(avg_json, sizeof(avg_json),
+         "{\"avg_wifi\":%.2f,\"avg_mqtt_connect\":%.2f,\"avg_mqtt_publish\":%.2f,\"avg_capture\":%.2f}",
+         avg_wifi, avg_mqtt_connect, avg_mqtt_publish, avg_capture);
+
+        esp_mqtt_client_publish(client, MQTT_BROKER_TOPIC_AVERAGE, avg_json, strlen(avg_json), 0, 0);
+        ESP_LOGI(TAG, "Averages published: %s", avg_json);
+
+        // Reset samples
+        reset_nvs_samples();
+        sample_count = 0;
+    }
+
+    sent_successfully = true;
+
+cleanup:
+    if (client) {
+        esp_mqtt_client_stop(client);
+        esp_mqtt_client_destroy(client);
+    }
     ESP_ERROR_CHECK(app_wifi_disconnect());
-
     ESP_ERROR_CHECK(app_wifi_deinit());
+#endif
 
-    ESP_LOGI(TAG, "End of program");
+    if (sent_successfully) {
+        ESP_LOGI(TAG, "Operation successful. Going to deep sleep for 10 seconds...");
+    } else {
+        ESP_LOGE(TAG, "Operation failed. Going to deep sleep for 10 seconds...");
+    }
+
+    // Deep sleep for 10 seconds
+    esp_deep_sleep(10 * 1000000);
+}
+
+// Save individual timing to NVS
+void save_timing_to_nvs(const timing_data_t *data, int index)
+{
+    nvs_handle_t nvs;
+    char key[32];
+
+    ESP_ERROR_CHECK(nvs_open("timing", NVS_READWRITE, &nvs));
+
+    snprintf(key, sizeof(key), "wifi_%d", index);
+    ESP_ERROR_CHECK(nvs_set_i64(nvs, key, data->wifi_time));
+
+    snprintf(key, sizeof(key), "mqtt_connect_%d", index);
+    ESP_ERROR_CHECK(nvs_set_i64(nvs, key, data->mqtt_connect_time));
+
+    snprintf(key, sizeof(key), "mqtt_publish_%d", index);
+    ESP_ERROR_CHECK(nvs_set_i64(nvs, key, data->mqtt_publish_time));
+
+    snprintf(key, sizeof(key), "capture_%d", index);
+    ESP_ERROR_CHECK(nvs_set_i64(nvs, key, data->capture_time));
+
+    ESP_ERROR_CHECK(nvs_commit(nvs));
+    nvs_close(nvs);
+}
+
+// Load timing from NVS
+void load_timing_from_nvs(timing_data_t *data, int index)
+{
+    nvs_handle_t nvs;
+    char key[32];
+
+    ESP_ERROR_CHECK(nvs_open("timing", NVS_READWRITE, &nvs));
+
+    snprintf(key, sizeof(key), "wifi_%d", index);
+    ESP_ERROR_CHECK(nvs_get_i64(nvs, key, &data->wifi_time));
+
+    snprintf(key, sizeof(key), "mqtt_connect_%d", index);
+    ESP_ERROR_CHECK(nvs_get_i64(nvs, key, &data->mqtt_connect_time));
+
+    snprintf(key, sizeof(key), "mqtt_publish_%d", index);
+    ESP_ERROR_CHECK(nvs_get_i64(nvs, key, &data->mqtt_publish_time));
+
+    snprintf(key, sizeof(key), "capture_%d", index);
+    ESP_ERROR_CHECK(nvs_get_i64(nvs, key, &data->capture_time));
+
+    nvs_close(nvs);
+}
+
+// Clear all timing data in NVS
+void reset_nvs_samples(void)
+{
+    nvs_handle_t nvs;
+    char key[32];
+
+    ESP_ERROR_CHECK(nvs_open("timing", NVS_READWRITE, &nvs));
+
+    for (int i = 0; i < TIMING_SAMPLES; i++) {
+        snprintf(key, sizeof(key), "wifi_%d", i);
+        nvs_erase_key(nvs, key);
+
+        snprintf(key, sizeof(key), "mqtt_connect_%d", i);
+        nvs_erase_key(nvs, key);
+
+        snprintf(key, sizeof(key), "mqtt_publish_%d", i);
+        nvs_erase_key(nvs, key);
+
+        snprintf(key, sizeof(key), "capture_%d", i);
+        nvs_erase_key(nvs, key);
+    }
+
+    nvs_erase_key(nvs, "sample_count");
+    ESP_ERROR_CHECK(nvs_commit(nvs));
+    nvs_close(nvs);
 }
